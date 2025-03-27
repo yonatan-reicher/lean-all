60c7b01a301aff7d70e05084e02a94df7f195c00
Yonatan Reicher <yony252525@gmail.com>
2025-03-27 13:48:43 +0200

    Initial

---
+++ LeanAll.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LeanAll` library.
+-- Import modules here that should be built as part of the library.
+import LeanAll.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LeanAll
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
